2088a714d827875bc67ff07dd7c8ec84b90ef2c4
Dios Vo <vtmn1212@gmail.com>
2021-03-30 23:46:23 +0700

    Added Video Player: indicator & fragment

--- Storage/app/src/main/java/com/vtmn/storage/MainActivity.java
+++ Storage/app/src/main/java/com/vtmn/storage/MainActivity.java
@@ -1,14 +1,32 @@
 package com.vtmn.storage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecycleViewAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager((new LinearLayoutManager(this)));
+
+//        If lack in scrolling
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemViewCacheSize(20);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        recyclerView.setNestedScrollingEnabled(false);
+
+        recyclerAdapter = new RecycleViewAdapter(this);
+        recyclerView.setAdapter(recyclerAdapter);
     }
 }
